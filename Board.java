f951949fd5a09c2a0076171d48d3a11172b03c89
martisltu <dabr.martynas@gmail.com>
2019-02-27 18:22:54 +0000

    - Added location(x,y coordinates) to the Square object - Added some comments and documentation - Finishing touches to the loadSquares() function - Initial Square values are now set in a constructor - Added a .gitignore function ( to ignore .class files)

--- Board.java
+++ Board.java
@@ -1,3 +1,15 @@
+/*
+----board layout----
+-   y
+- x 0 1 2 3 4 5 6 7
+-   1 * * * * * * *
+-   2 * * * * * * *
+-   3 * * * * * * *
+-   4 * * * * * * *
+-   5 * * * * * * *
+-   6 * * * * * * *
+-   7 * * * * * * *
+*/
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -6,46 +18,50 @@ public class Board
 {
     JFrame a = new JFrame(); // Create a blank window
     JPanel panel = new JPanel(); // Create a panel
-    GridLayout layout = new GridLayout(8,8);
+    GridLayout layout = new GridLayout(8,8); // create an 8x8 grid layout
     public Board()
     {
         a.setContentPane(panel); // Use panel on Window
-        panel.setLayout(layout);
+        panel.setLayout(layout); // Set layout
         a.setVisible(true); // Make it visible
         a.setTitle("Draughts"); // Change window title
         a.setSize(800,800); // Change window size
-        a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        a.setResizable(false);
+        a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit program on close
+        a.setResizable(false); // Make the window non-resizable
     }
+    /**
+    * Creates 64 squares, sets their colour and arranges them in an 8x8 grid.
+    */
     void loadSquares()
     {
-        Square[] square = new Square[64];
-        ImageIcon white = new ImageIcon("img/empty.png");
-        ImageIcon black = new ImageIcon("img/empty2.png");
+
+        Square[] square = new Square[64]; // Init 64 squares
+        ImageIcon white = new ImageIcon("img/empty.png"); // Create white ImageIcon
+        ImageIcon black = new ImageIcon("img/empty2.png"); // Create black ImageIcon
+        int x = 0; // Create x coordinate
+        int y = 0; // Create y coordinate
         for(int i = 0;i<64;i=i+8)
         {
-            for(int a = i;a<i+8;a=a+2)
+            for(int a = i;a<i+8;a+=2)
             {
-                square[a] = new Square();
-                square[a].button.setIcon(black);
-                panel.add(square[a].button);
-                square[a+1] = new Square();
-                square[a+1].button.setIcon(white);
-                panel.add(square[a+1].button);
-                System.out.println("printed 2");
+                square[a] = new Square(black,x,y); // Add a black square
+                panel.add(square[a].button); // Add square to Jpanel
+                square[a+1] = new Square(white,x+1,y); // Add a white square
+                panel.add(square[a+1].button); // Add square to Jpanel
+                x+=2; // Added 2 squares, so x=x+2
             }
-            i=i+8;
-            for(int a = i;a<i+8;a=a+2)
+            i+=8; // Added a row of squares, so i=i+8
+            for(int a = i;a<i+8;a+=2)
             {
-                square[a] = new Square();
-                square[a].button.setIcon(white);
-                panel.add(square[a].button);
-                square[a+1] = new Square();
-                square[a+1].button.setIcon(black);
-                panel.add(square[a+1].button);
+                square[a] = new Square(white,x,y+1); // Add a white square
+                panel.add(square[a].button); // Add square to Jpanel
+                square[a+1] = new Square(black,x+1,y+1); // Add a black square
+                panel.add(square[a+1].button); // Add square to Jpanel
+                x+=2; // Added 2 squares, so x=x+2
             }
+            y+=2; // Added two rows, so y=y+2
         } 
-        a.setVisible(true); // Make it visible
+        a.setVisible(true); // Make layout visible
     }
 
 }
--- Square.java
+++ Square.java
@@ -1,6 +1,44 @@
+/*
+----board layout----
+-   y
+- x 0 1 2 3 4 5 6 7
+-   1 * * * * * * *
+-   2 * * * * * * *
+-   3 * * * * * * *
+-   4 * * * * * * *
+-   5 * * * * * * *
+-   6 * * * * * * *
+-   7 * * * * * * *
+*/
 import javax.swing.*;
 public class Square
 {
-    ImageIcon white = new ImageIcon("img/empty2.png");
-    JButton button = new JButton();
+    private int xPosition;
+    private int yPosition;
+    JButton button;
+    /**
+    * Square constructor
+    * @param color is the color of the square
+    * @param x is the x position of the square
+    * @param y is the y position of the square
+    */
+    public Square(ImageIcon color, int x, int y)
+    {
+        button = new JButton(color);
+        button.setIcon(color);
+        xPosition = x;
+        yPosition = y;
+    }
+    /**
+     * @return the xPosition
+     */
+    public int getxPosition() {
+        return xPosition;
+    }
+    /**
+     * @return the yPosition
+     */
+    public int getyPosition() {
+        return yPosition;
+    }
 }
